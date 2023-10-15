4c453ba179f3865ab633cf92c6ddcd53628fa013
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-10-15 14:23:25 +0200

    Added the prevention of the default form action

--- scripts/AdminPanelPageScript.js
+++ scripts/AdminPanelPageScript.js
@@ -2,6 +2,8 @@ document.getElementById('clients-portal').addEventListener('click', () => {
     window.location.href = '/clientsPortalPage';
 });
 
-document.getElementById('search-user').addEventListener('submit', () => {
+document.getElementById('search-user').addEventListener('submit', (event) => {
+    //Prevent the default form action
+    event.preventDefault();
 
 });
