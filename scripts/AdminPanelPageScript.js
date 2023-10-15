bfd83a7ad68c1a4fbc978e75d954c2f7a567cb71
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-10-15 15:33:21 +0200

    Added an extraction of the desired user's email from the admin panel

--- scripts/AdminPanelPageScript.js
+++ scripts/AdminPanelPageScript.js
@@ -6,5 +6,6 @@ document.getElementById('search-user').addEventListener('submit', (event) => {
     //Prevent the default form action
     event.preventDefault();
 
-
+    //Retrieve the desired user's email
+    const email = document.getElementById('email').value;
 });
