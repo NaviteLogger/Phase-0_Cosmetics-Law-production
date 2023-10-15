3683ed7de742ce18e9555eb610e3dc18fc6c8f6c
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-10-15 15:09:18 +0200

    Added a form to chose an action from (admin panel)

--- scripts/AdminPanelPageScript.js
+++ scripts/AdminPanelPageScript.js
@@ -2,8 +2,9 @@ document.getElementById('clients-portal').addEventListener('click', () => {
     window.location.href = '/clientsPortalPage';
 });
 
-document.getElementById('search-user').addEventListener('submit', (event) => {
+document.getElementById('admin-panel-action-choice').addEventListener('submit', (event) => {
     //Prevent the default form action
     event.preventDefault();
+
 
 });
