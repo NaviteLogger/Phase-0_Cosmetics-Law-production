16c41f2815e25d36b92629a56ad024c3115eb307
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-10-15 15:36:48 +0200

    Added a POST request with the desired email on the client side of the AdminPanel

--- scripts/AdminPanelPageScript.js
+++ scripts/AdminPanelPageScript.js
@@ -8,4 +8,27 @@ document.getElementById('search-user').addEventListener('submit', (event) => {
 
     //Retrieve the desired user's email
     const email = document.getElementById('search-user-input').value;
+
+    //Send a POST request to the server
+    fetch('/searchUser', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email: email }), //Indicate that we are sending JSON data in the request body
+    })
+        .then((response) => response.json()) //Parse the incoming JSON response
+        .then((data) => {
+            const serverMessageElement = document.getElementById('message');
+            serverMessageElement.innerHTML = data.message;
+
+            if (data.status === 'user_found') {
+                setTimeout(() => {
+                    window.location.href = '/userPage';
+                }, 1500); //Redirect to the user page after 1,5 seconds
+            }
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
 });
