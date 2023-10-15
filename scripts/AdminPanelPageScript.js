770d8cb3918c0679e33c971b25b378b8e9e0ba83
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-10-15 14:19:57 +0200

    Added an event listener for the search user section

--- scripts/AdminPanelPageScript.js
+++ scripts/AdminPanelPageScript.js
@@ -1,3 +1,7 @@
 document.getElementById('clients-portal').addEventListener('click', () => {
     window.location.href = '/clientsPortalPage';
 });
+
+document.getElementById('search-user').addEventListener('submit', () => {
+
+});
